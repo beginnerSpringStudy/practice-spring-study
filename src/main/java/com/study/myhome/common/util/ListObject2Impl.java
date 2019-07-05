0da0118dd42d46f0584ab0912d4d40b1430abd04
kkforgg@naver.com <jms>
2019-07-05 23:41:32 +0900

    사용자 리스트 검색 추가

--- src/main/java/com/study/myhome/common/util/ListObject2Impl.java
+++ src/main/java/com/study/myhome/common/util/ListObject2Impl.java
@@ -2,9 +2,15 @@ package com.study.myhome.common.util;
 
 import java.util.List;
 
+import com.study.myhome.board.service.BoardVO;
+
 import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 
 public class ListObject2Impl<T> implements ListObject<T> {
+
+	public ListObject2Impl(List<BoardVO> list, int totalCount, PaginationInfo paginationInfo, int i) {
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public List<T> getList() {
